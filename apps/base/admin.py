caaa1b87dab1f781b14d16793121015bb2051ca3
Arun Kumar <arunkakorp@gmail.com>
2018-02-25 09:37:25 +0530

    Updates to the admin section
    Made Profile fields favourite_books and authors readonly, so as
    not to incur Author and Book query cost.
    
    Added Payment link field on user in cases where payment_request_id does
    not exist

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from .models import Genre, Publisher, Author, Book, Profile, Item, Payment
 
-for m in [Genre, Publisher, Profile, Item]:
+for m in [Genre, Publisher, Item]:
     admin.site.register(m)
 
 @admin.register(Author)
@@ -16,8 +16,17 @@ class BookAdmin(admin.ModelAdmin):
     fields = ['title', 'ol_id', 'isbn', 'genres', 'authors', 'publisher', 'year_of_publication']
     list_display = ['title', 'ol_id']
     autocomplete_fields = ['authors']
+    search_fields = ['title']
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     fields = ['user', 'item', 'payment_date', 'payment_id', 'payment_request_id', 'amount', 'fees', 'status', 'longurl' ,'shorturl']
     list_display = ['payment_request_id', 'user', 'item', 'payment_date', 'amount', 'status']
+    list_display_links = ['payment_request_id', 'user']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ('user', 'address', 'city', 'state', 'postal_code', ('phone_mobile', 'phone_landline'),
+            'genres', 'favourite_books', 'favourite_authors', 'interests', 'boxes_remaining')
+    readonly_fields = ( 'favourite_books', 'favourite_authors', 'interests' )
+
